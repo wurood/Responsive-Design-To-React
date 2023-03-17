dd02c066b53bdc2db271bb836e0025e006f4ea4c
unknown <wuroodj95@gmail.com>
2023-03-17 20:13:10 +0200

    Change Responsive To React update

--- src/components/Header/HeaderLeft/index.js
+++ src/components/Header/HeaderLeft/index.js
@@ -5,8 +5,8 @@ import Paragraph from '../../Paragraph/index'
 function HeaderLeft() {
   return (
     <div>
-    <Title text="Living the social life" type="h1" />
-    <Paragraph text="a blog exploring minimalism in life"/>
+      <Title text="Living the social life" type="h1" />
+      <Paragraph text="a blog exploring minimalism in life"/>
     </div>
   )
 }
